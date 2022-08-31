f830870fda4b3dcc17295dcc9e158000fac6cdf3
salesfj <saleswebd@gmail.com>
2022-08-31 00:15:29 -0300

    novo deploy

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const express = require('express');
 const app = express();
 
 app.get('/',(request,response)=>{
-return response.json({message:'Server is up'});
+return response.json({message:'Server está funcionando'});
 })
 
 app.get('/atualizou',(request, response)=>{
-    return response.json({message:'atualizou mesmo!'});
+    return response.json({message:'Atualizou mesmo!'});
 })
 
 app.post('/teste', (request, response)=>{
