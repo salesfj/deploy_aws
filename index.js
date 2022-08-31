3c001cd94d6ed2f18e3794438b15d4e31732797f
salesfj <saleswebd@gmail.com>
2022-08-30 22:27:24 -0300

    testando atualizacao no servidor

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/',(request,response)=>{
 return response.json({message:'Server is up'});
 })
 
+app.get('/atualizou',(request, response)=>{
+    return response.json({message:'atualizou mesmo!'});
+})
+
 app.post('/teste', (request, response)=>{
     const {name, date} = request.body;
 })
